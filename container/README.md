e9c3508f6881337029d8420fa762dc9665f9d4f6
Hugh Secker-Walker <hsw@20ocatves.com>
2025-01-06 22:17:15 +0000

    feat(container): add POC for dusk/rusk podman container

---
+++ container/README.md
@@ -0,0 +1,3 @@
+# Dusk Container
+
+Proof of concept for running dusk/rusk in a container (podman) with dusk/rusk state being stored in a directory on the host.
---
+++ container/initialize-dusk-state.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -euo pipefail
+
+dusk_src_dir=/opt/dusk.install
+wallet_src_dir=/root/.dusk.install
+
+dusk_target_dir=/opt/dusk
+wallet_target_dir=/root/.dusk
+
+#mount
+
+# copy the stateful trees, create the same nonce in each tree
+if [ \( ! -e $dusk_target_dir -o -z "$(ls -A $dusk_target_dir)" \) -a \( ! -e $wallet_target_dir  -o -z "$(ls -A $wallet_target_dir)" \) ] ; then
+    timestamp=$(date '+%Y-%m-%d-%H%M-%S')
+    echo timestamp: $timestamp
+    tag=$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM
+    echo tag: $tag
+
+    nonce=${timestamp}_${tag}
+    echo nonce: $nonce
+
+    cp -a $dusk_src_dir/. $dusk_target_dir
+    echo $nonce | tee $dusk_target_dir/nonce
+
+    cp -a $wallet_src_dir/. $wallet_target_dir
+    echo $nonce | tee $wallet_target_dir/nonce
+fi
+
+set -x
+
+# we don't try to dissect possible reasons for failure...
+cmp $dusk_target_dir/nonce $wallet_target_dir/nonce
+
+echo RUSK_PUBLIC_ADDRESS: $RUSK_PUBLIC_ADDRESS
+
+sed -i \
+    -e "s/# log_type = 'json'/log_type = 'json'/" \
+    $dusk_target_dir/conf/rusk.toml
+
+#    -e "s/ *public_address *=.*/public_address = '$RUSK_PUBLIC_ADDRESS'/" \
